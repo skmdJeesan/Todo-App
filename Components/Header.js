4c0848cc7b1c2c30a41dea643836b8ae5bd93716
skmdJeesan <skmdjeesan@gmail.com>
2025-09-13 19:43:04 +0530

    header update

--- Components/Header.js
+++ Components/Header.js
@@ -3,8 +3,8 @@ import Livetime from './Livetime'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center bg-black gap-120 py-5'>
-      <h1 className='text-amber-500 text-2xl font-extrabold uppercase ml-140'>My Todo list</h1>
+    <div className='flex flex-col items-center justify-center bg-black py-4'>
+      <h1 className='text-amber-500 text-2xl font-extrabold uppercase'>My Todo list</h1>
       <Livetime locale="en-IN" hour12={false}/>
     </div>
   )
